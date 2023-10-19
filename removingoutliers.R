87d49c9ed6ac273011a959b717d6b3028c065a13
harshita9817 <143436006+harshita9817@users.noreply.github.com>
2023-10-19 12:06:28 -0500

    Update removingoutliers.R

--- removingoutliers.R
+++ removingoutliers.R
@@ -1,17 +1,29 @@
+install.packages("readxl")
+library(readxl)
 # dataset for flowering-time
-ft_df <- read.csv("SAP2021.csv")
+ft_df <- read_excel("SAPMerged2021_v2.3.xlsx")
+#Removing additional columns
+head(ft_df)
+ft_df$...12<-NULL
+ft_df$...13<-NULL
 
-# Calculate quartiles and IQR
-Q1 <- quantile(data, 0.25)
-Q3 <- quantile(data, 0.75)
+# Remove rows with NA values in the 'DaysToFlower' column
+ft_df2<- subset(ft_df, !is.na(DaysToFlower))
+
+# Calculate Q1 and Q3 for the 'days_to_flower' column
+Q1 <- quantile(ft_df2$DaysToFlower, 0.25)
+Q3 <- quantile(ft_df2$DaysToFlower, 0.75)
+
+# Calculate IQR for the 'days_to_flower' column
 IQR <- Q3 - Q1
 
-# Define lower and upper bounds
+# Determine lower and upper bounds to identify outliers
 lower_bound <- Q1 - 3 * IQR
 upper_bound <- Q3 + 3 * IQR
 
-# Remove outliers
-filtered_data <- ft_df[ft_df >= lower_bound & ft_df <= upper_bound]
+# Remove rows with 'DaysToFlower' values outside the lower and upper bounds
+ft_df3 <- ft_df2[ft_df2$DaysToFlower >= lower_bound & ft_df2$DaysToFlower <= upper_bound, ]
 
-print("Original Data:", ft_df)
-print("Data without outliers:", filtered_data)
+#to save the file in the directory(set the working directory if required to change)
+write.csv(ft_df, "Original Data.csv", row.names = FALSE)
+write.csv(ft_df3, "Data without outliers.csv", row.names = FALSE)
